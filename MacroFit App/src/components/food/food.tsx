60d14b1dd7d4c2782e8cdd566a402eb0a5c915e0
SalvadoRobles03 <salvadorobles03@gmail.com>
2024-11-03 14:19:38 -0600

    📦 NEW: MF-30 | Testing Progress Circle

--- MacroFit App/src/components/food/food.tsx
+++ MacroFit App/src/components/food/food.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { View, Text } from "react-native";
 import { Surface } from "react-native-paper";
 import tw from "tailwind-react-native-classnames";
+import CircularProgress from 'react-native-circular-progress-indicator';
 
 const Food = () => {
   return (
     <Surface
       style={tw`flex-1 justify-center items-center h-full w-full bg-black`}
     >
-      <Text style={tw`text-white text-lg`}>PAGE 2</Text>
+      <CircularProgress value={60} />
     </Surface>
   );
 };
